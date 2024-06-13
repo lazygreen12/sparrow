40a1d37aa7aa5d3db4022c08ae86a0466114fda1
懒小绿 <lazygreen@littleGreenMac.local>
2024-06-13 17:11:57 +0800

    chore: init local env

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2023,
+    sourceType: 'module',
+  },
+  rules: {
+    'import/prefer-default-export': 0,
+  },
+};
